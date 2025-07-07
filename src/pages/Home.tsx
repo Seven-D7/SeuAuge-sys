a3ede7ee56460ea3219a35d391ae463a49c7d0e6
Seven-D7 <d7.seven.d7@gmail.com>
2025-07-07 10:03:12 -0300

    att

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react';    
+import './Home.css';
 import { Button } from './components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card';
 import { Badge } from './components/ui/badge';
